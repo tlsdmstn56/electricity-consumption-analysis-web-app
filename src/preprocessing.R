8f719d3e15ca607b7fbb9f7e4edfa72ed71ebadb
eunsoo sheen <eunsoo.sheen@gmail.com>
2018-08-17 14:01:18 -0700

    change directory structure

---
+++ src/preprocessing.R
@@ -0,0 +1,100 @@
+library(readr)
+library(dplyr)
+
+# codebook preprocessing
+codebook <- readxl::read_xlsx("../data/codebook2015_public_v3.xlsx",skip = 3)
+codebook <- data.frame(name=codebook$`SAS Variable Name`,
+                      desc=codebook$`Variable Description`,
+                      coded=codebook$X__1,
+                      cat=codebook$`Final Response Set`)
+cat.table <- rep(0,NROW(codebook))
+for(i in 1:NROW(codebook)) {
+  code <- strsplit(as.character(codebook[i,3]),"\r\n")
+  cat <- strsplit(as.character(codebook[i,4]),"\r\n")
+  rs <- ""
+  if (is.na(code)){
+    for(j in 1:NROW(cat[[1]])){
+      rs <- paste(rs,cat[[1]][j],sep="",end="\n")
+    }
+  } else {
+    for(j in 1:NROW(cat[[1]])){
+      rs <- paste(rs,code[[1]][j],"\t",cat[[1]][j],sep="",end="\n")
+    }
+  }
+  cat.table[i]=rs
+}
+codebook_final <- data.frame(name=unlist(codebook$name),
+                            desc=unlist(codebook$desc),
+                            coded=unlist(cat.table))
+
+drop.codebook.row <- function(data, pattern, print_result=FALSE) {
+  test <- data[-grep(pattern, unlist(data$name)), ]
+  if (print_result) {
+    cat(NROW(data),"-->",NROW(test),end="\n")
+  }
+  return(test)
+}
+
+codebook_final <- drop.codebook.row(codebook_final,"CUFEET")
+codebook_final <- drop.codebook.row(codebook_final,"BRRWT")
+codebook_final <- drop.codebook.row(codebook_final,"BTUNG")
+codebook_final <- drop.codebook.row(codebook_final,"GALLON")
+codebook_final <- drop.codebook.row(codebook_final,"DOEID")
+codebook_final <- drop.codebook.row(codebook_final,"REGIONC")
+codebook_final <- drop.codebook.row(codebook_final,"^Z+")
+codebook_final <- drop.codebook.row(codebook_final,"TOTU")
+codebook_final <- drop.codebook.row(codebook_final,"NWEIGHT")
+codebook_final <- drop.codebook.row(codebook_final,"CDD")
+codebook_final <- drop.codebook.row(codebook_final,"HDD")
+codebook_final <- drop.codebook.row(codebook_final,"DOLNG")
+codebook_final <- drop.codebook.row(codebook_final,"BTULP")
+codebook_final <- drop.codebook.row(codebook_final,"DOLLP")
+codebook_final <- drop.codebook.row(codebook_final,"BTUFO")
+codebook_final <- drop.codebook.row(codebook_final,"DOLFO")
+codebook_final <- drop.codebook.row(codebook_final,"TOTAL")
+codebook_final <- drop.codebook.row(codebook_final,"FUEL")
+codebook_final <- drop.codebook.row(codebook_final,"^TOT*")
+
+codebook_final <- codebook_final[1:335,]
+write.csv(codebook_final,"../data/codebook_final.csv",row.names=FALSE)
+
+
+# drop unnecessary columns in raw dataset
+data <- read_csv("./data/recs2015_public_v3.csv", locale=locale(tz="US/Pacific"))
+drop.column.re <- function(data, pattern, ask=FALSE){
+  # iteractively drop column
+  test <- data[, -grep(pattern, colnames(data))]
+  if(!ask){
+    return(test)
+  } 
+  # checking how many column will be dropped
+  change <- paste(dim(data)[2],"->",dim(test)[2],sep=" ")
+  res <- readline(prompt=paste(change,"Confirm[y/n]: ",sep="\n"))
+  if(res=="y"){
+    return(test)
+  } else {
+    return(data)
+  }
+}
+
+data <- drop.column.re(data,"CUFEET")
+data <- drop.column.re(data,"BRRWT")
+data <- drop.column.re(data,"BTUNG")
+data <- drop.column.re(data,"GALLON")
+data <- drop.column.re(data,"DOEID")
+data <- drop.column.re(data,"REGIONC")
+data <- drop.column.re(data,"^Z+")
+data <- drop.column.re(data,"TOTU")
+data <- drop.column.re(data,"NWEIGHT")
+data <- drop.column.re(data,"CDD",ask=F)
+data <- drop.column.re(data,"HDD",ask=F)
+data <- drop.column.re(data,"DOLNG")
+data <- drop.column.re(data,"BTULP")
+data <- drop.column.re(data,"DOLLP")
+data <- drop.column.re(data,"BTUFO")
+data <- drop.column.re(data,"DOLFO")
+data <- drop.column.re(data,"TOTAL")
+data <- drop.column.re(data,"FUEL")
+data <- drop.column.re(data,"^TOT*")
+
+write.csv(data,"./data/recs2015_clean.csv",row.names = FALSE)
---
+++ src/ui.R
@@ -0,0 +1,158 @@
+# Define UI for application
+#
+# titlePanel
+# textOutPut(description pages)
+# navibar
+#   tabPanel(Total Consumption)
+#     [sidebarLayout]
+#      sidebarPanel(two input dropdowns)
+#      mainPanel
+#       [tabsetPanel]
+#        tabPanel(Plot)
+#        tabPanel(Summary Table)
+#        tabPanel(Anova Test)
+#   tabPanel(Consumption Usage)
+#     [sidebarLayout]
+#      sidebarPanel(one input dropdowns)
+#      mainPanel
+#       [tabsetPanel]
+#        tabPanel(Box Plot)
+#        tabPanel(Bar Chart)
+#        tabPanel(Summary Table)
+#   tabPanel(Codebook)
+ui <- fluidPage(
+  # title
+  titlePanel("Consumption Analysis", windowTitle="Consumption Analysis"),
+  # guide(frequent used variables, data source, some infos)
+  tagList(
+    p("Data Source: ",
+      a(href="https://www.eia.gov/consumption/residential/data/2015/", 
+        "RECS2015(Residential Electricity Consumption Survey 2015)")),
+    tags$ul(
+      tags$li("MONEYPY: income"),
+      tags$li("CLIMATE_REGION_PUB: climate(cold, very cold, hot dry,...)"),
+      tags$li("TYPEHUQ: Type of housing unit(mobile, single-family, apartment..)"),
+      tags$li("UATYP10: Census 2010 Urban Type(Urban, Rural)"),
+      tags$li("DIVISION: Census Division(New Englance, Pacific..)"),
+      tags$li("HHSEX: Respondent gender"),
+      tags$li("HHAGE: Respondent age"),
+      tags$li("EMPLOYHH: Respondent employment status(fulltime, part time..)")
+    ),
+    p("eunsoo.sheen@encoredtech.com for any feedback")
+  ),
+  # Navigation Bar on the top
+  navbarPage(title = "Menu",
+             # Total Consumption Tab
+             tabPanel("Total Consumption",
+                      sidebarLayout(
+                        sidebarPanel(
+                          h3("Auto-sizing Plot"),
+                          switchInput(inputId = "p1_box_autosize", value=TRUE),
+                          conditionalPanel(condition = "!input.p1_box_autosize",
+                                           sliderInput("p1_box_height", "Height:",
+                                                       min = 0, max = 1000,
+                                                       value = 600),
+                                           sliderInput("p1_box_width", "Width:",
+                                                       min = 0, max = 2000,
+                                                       value = 800)),
+                          hr(),
+                          h3("Variables"),
+                          # first group
+                          selectInput("p1_criterion1", 
+                                      "X variable",
+                                      DROPDOWN_MENU),
+                          # second group
+                          selectInput("p1_criterion2", 
+                                      "Color Variable",
+                                      c("None",DROPDOWN_MENU)),
+                          # group description
+                          htmlOutput("p1_var_desc")
+                          
+                          
+                          ),
+                        # plots and summary, anova
+                        mainPanel(
+                          tabsetPanel(
+                            tabPanel("Box Plot",
+                                     plotlyOutput("p1_boxplot") %>% withSpinner
+                            ),
+                            tabPanel("Summary",
+                                     verbatimTextOutput("p1_summary")),
+                            tabPanel("Anova Test", 
+                                     verbatimTextOutput("p1_aov")),
+                            id = "p1_tabs"
+                          )) # end of main panel
+                      ),
+                      value="p1"),
+             # Consumption Usage tab
+             tabPanel("Consumption Usage",
+                      sidebarLayout(
+                        # for group selection
+                        sidebarPanel(
+                          selectInput("p2_criterion", "Criterion",
+                                      sort(COLNAMES_IN_DROPDOWN[1:242])),
+                          htmlOutput("p2_var_desc"),
+                          htmlOutput("p2_legend")),
+                        mainPanel(
+                          tabsetPanel(
+                            tabPanel("Box Plot",
+                                     br(),
+                                     tags$p("Press Download Button for bigger image"),
+                                     tags$div(
+                                       downloadButton("p2_download_boxplot_split", 
+                                                      "Download(file by plots, .zip)"), 
+                                       downloadButton("p2_download_boxplot_combined", 
+                                                      "Download(combined to one file, .jpeg)")
+                                     ),
+                                     br(),
+                                     plotlyOutput("p2_boxplot") %>% withSpinner,
+                                     value = "box"
+                                     
+                            ) ,
+                            
+                            tabPanel("Bar Chart",
+                                     br(),
+                                     tags$p("Press Download Button for bigger image"),
+                                     tags$div(
+                                       downloadButton("p2_download_barplot_split", 
+                                                      "Download(file by plots, .zip)"), 
+                                       downloadButton("p2_download_barplot_combined", 
+                                                      "Download(combined to one file, .jpeg)")
+                                     ),
+                                     br(),
+                                     plotlyOutput("p2_barplot"
+                                                  #, width=1500, height=700
+                                                  ) %>% 
+                                       withSpinner,
+                                     value = "bar"),
+                            tabPanel("Summary Table", 
+                                     htmlOutput("p2_summary_header_avg"),
+                                     br(),
+                                     tags$div(
+                                       downloadButton("p2_download_summary_avg", 
+                                                      "Download(average, .csv)")
+                                     ),
+                                     br(),
+                                     tableOutput("p2_summary_avg"),
+                                     
+                                     br(),
+                                     htmlOutput("p2_summary_header_percentage"),
+                                     br(),
+                                     tags$div(
+                                       downloadButton("p2_download_summary_percentage",
+                                                      "Download(percentage, csv)")
+                                     ),
+                                     br(),
+                                     tableOutput("p2_summary_percentage"),
+                                     value = "summary"
+                                    ),
+                            id="p2_tabs"
+                            ) # end of p2 tabsetpanel
+                          ) # end of main panel
+                      ),
+                      value="p2"),
+             # code book for variable description
+             tabPanel("Codebook", dataTableOutput('p3_codebook'),
+                      value="p3"),
+             id = 'main_nav')
+)
